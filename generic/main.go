d13f52181c36db83bc21ab2521f190f34b3cc510
leandroKachu <lesilvajunior@hotmail.com>
2023-03-30 15:10:14 -0700

    add a exercise for generic method

---
+++ generic/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	slice2 := []string{"a", "b", "c", "d"}
+	slice1 := []int{1, 2, 3, 4}
+
+	newInts := reverse(slice1)
+	newString := reverse(slice2)
+
+	fmt.Println(newInts)
+	fmt.Println(newString)
+
+}
+
+// para nao ter de criar duas funcoes para fazer do tipo int e string, o generic metodo e esse [T int | T string]
+func reverse[T int | string](slice []T) []T {
+	newInts := make([]T, len(slice))
+
+	newIntsLen := len(slice) - 1
+	for i := 0; i < len(slice); i++ {
+		newInts[newIntsLen] = slice[i]
+		newIntsLen--
+	}
+	return newInts
+}
